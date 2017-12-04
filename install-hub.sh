e36c5f0b08249cfb162adfc52c16178b8a7a192f
Jeremy Valance <jvalance@almos-iphone.bds-ad.lc>
2017-12-04 16:50:16 -0500

    Updated shell script

--- install-hub.sh
+++ install-hub.sh
@@ -6,7 +6,6 @@ sudo apt-get -y install default-jre
 wget https://github.com/blackducksoftware/hub/archive/v4.3.1.tar.gz
 tar xvf v4.3.1.tar.gz
 
-cd hub-4.3.1
-cd docker-swarm
+cd hub-4.3.1/docker-swarm
 sudo docker swarm init
 sudo docker stack deploy -c docker-compose.yml hub
