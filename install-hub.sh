7449a20d6c9b2702fae51a0f444922dd09fe2e1f
Jeremy Valance <jvalance@almos-iphone.bds-ad.lc>
2017-12-04 16:25:40 -0500

    Updated hub install script

--- install-hub.sh
+++ install-hub.sh
@@ -1,3 +1,12 @@
-#Install Hub
+## Install Java
+sudo apt-get update 
+sudo apt-get -y install default-jre
+
+## Install Hub
 wget https://github.com/blackducksoftware/hub/archive/v4.3.1.tar.gz
-tar xvf hub-4.3.1.tar
+tar xvf v4.3.1.tar.gz
+
+cd hub-4.3.1
+cd docker-swarm
+sudo docker swarm init
+sudo docker stack deploy -c docker-compose.yml hub
