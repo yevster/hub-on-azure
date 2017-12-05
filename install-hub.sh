47ed43fad0ac0c4c492fbacdf5bccb134aafaf44
Yev Bronshteyn <yevster@gmail.com>
2017-12-05 14:22:36 -0500

    Update install-hub.sh
    Adding instructions to install docker (needed for default cloud image)

--- install-hub.sh
+++ install-hub.sh
@@ -1,6 +1,24 @@
+## Install Docker (https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/#set-up-the-repository)
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce=17.09.0~ce-0~ubuntu
+sudo service docker start
+
+
 ## Install Java
-sudo apt-get update 
 sudo apt-get -y install default-jre
+
+
 
 ## Install Hub
 cd /
