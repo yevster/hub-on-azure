0b2c5b005262335232abc47cb43dea2101e0b59c
Yev Bronshteyn <ybronshteyn@blackducksoftware.com>
2017-12-06 13:58:47 -0500

    Fixing docker and compose installation.

--- install-hub.sh
+++ install-hub.sh
@@ -6,16 +6,17 @@ sudo apt-get install -y \
     curl \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+    sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
 sudo apt-get update
-sudo apt-get install docker-ce=17.09.0~ce-0~ubuntu
+sudo apt-get install -y docker-ce=17.09.0~ce-0~ubuntu
 sudo service docker start
 
 ## Install docker compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 cd /usr/bin
 sudo ln -s /usr/local/bin/docker-compose
 
